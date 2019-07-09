e4312a4bfa4fdd6319ddfda948b539f248a12248
Kevin Weaver <kevinweaver2@gmail.com>
2019-07-09 07:06:46 -0700

    Add description to MockRegistry

--- contracts/MockRegistry.sol
+++ contracts/MockRegistry.sol
@@ -1,5 +1,9 @@
 pragma solidity 0.5.7;
 
+/**
+ * @title MockRegistry
+ * @dev Will be replaced by the Humanity Registry on main net.
+ */
 contract MockRegistry {
   function isHuman(address who) external view returns (bool) {
     return true;
